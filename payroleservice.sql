e84c16f125becb2f70116675892b3f03073d8cf7
Ameejac <ameejacherukuru123@gmail.com>
2021-10-26 20:14:40 +0530

    [ameeja]added:creating employe payroleservice table

--- payroleservice.sql
+++ payroleservice.sql
@@ -1,3 +1,11 @@
 ---uc1 create payroleservice database---
 create database payrole_service;
 use payrole_service;
+
+---uc2 create employe payroleservice table---
+create table employee_payrole(
+id int identity(1,1) not null,
+name varchar (50) not null,
+salary  float not null,
+startdate date not null
+);
